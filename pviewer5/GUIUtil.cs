e86cd1d811644bc04f84fb1451a07ac5edf298f4
Chris Suchar <cmsuchar@verizon.net>
2015-12-10 04:48:07 -0500

    GUIUtil class created but still empty, ready to re-factor AGAIN

---
+++ pviewer5/GUIUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Text.RegularExpressions;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace pviewer5
+{
+    class GUIUtil
+    {
+    }
+}
