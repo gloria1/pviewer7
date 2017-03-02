2db2acc6773357f405f89028d52c0e57dde1e89e
csadmin <csadmin@DESKTOP-TTS9IRP>
2017-03-02 10:44:35 -0500

    wip

--- pviewer5/TestDataGrid.xaml.cs
+++ pviewer5/TestDataGrid.xaml.cs
@@ -34,47 +34,23 @@ namespace pviewer5
         public string timestamp { get; set; }
         public string ip { get; set; }
         public string proto { get; set; }
-        public string grouptype {get; set;}
-        public tdgitemlist parent { get; set; }
-       
-        public tdgitem(string t, string i, string p, string g, tdgitemlist par)
+        public object group { get; set; }
+        public Type grouptype { get { return group.GetType(); } }
+        public ObservableCollection<tdgitem> parent { get; set; }
+
+        public string grouped_ip { get; set; }
+        public string grouped_proto { get; set; }
+        public object grouped_group { get; set; }
+        public Type grouped_grouptype { get; set; }
+
+        public tdgitem(string t, string i, string p, object g, ObservableCollection<tdgitem> par)
         {
             timestamp = t;
             ip = i;
             proto = p;
-            grouptype = g;
+            group = g;
             parent = par;
-        }
-
-        // implement INotifyPropertyChanged
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void NotifyPropertyChanged(String propertyName = "")
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-    }
-
-    public class tdgitemlist : ObservableCollection<tdgitem>
-    {
-        public tdgitemlist()
-        {
-        }
-    }
-
-    public class tdgviewitem : INotifyPropertyChanged
-    {
-        public tdgitem item { get; set; }
-        public string grouped_ip { get; set; }
-        public string grouped_proto { get; set; }
-        public string grouped_grouptype { get; set; }
-
-        public tdgviewitem(tdgitem i)
-        {
-            item = i;
-            switch (i.ip)
+            switch (ip)
             {
                 case "192.168.11.222":
                     grouped_ip = "OTHER";
@@ -83,12 +59,41 @@ namespace pviewer5
                     grouped_ip = "OTHER";
                     break;
                 default:
-                    grouped_ip = i.ip;
+                    grouped_ip = ip;
                     break;
             }
-            grouped_proto = i.proto;
-            grouped_grouptype = null;
+            grouped_proto = proto;
+            grouped_group = group;
+            grouped_grouptype = grouptype;
         }
+
+        // implement INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+
+    public class tdggroupingaxis : INotifyPropertyChanged
+    {
+        public string propertyname { get; set; }
+        public string displayname { get; set; }
+        public bool ischecked { get; set; }
+        public ObservableCollection<tdggroupingaxis> parent;
+
+        public tdggroupingaxis(string pn, string dn, ObservableCollection<tdggroupingaxis> par)
+        {
+            propertyname = pn;
+            displayname = dn;
+            ischecked = true;
+            parent = par;
+        }
+
 
         // implement INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
@@ -104,40 +109,50 @@ namespace pviewer5
 
 
 
+
     public partial class TestDataGrid : Window
     {
-        public tdgitemlist l;
-        public ObservableCollection<tdgviewitem> vl { get; set; }
+        public ObservableCollection<tdgitem> vl { get; set; }
         public ListCollectionView view;
+
+        public ObservableCollection<tdggroupingaxis> axes { get; set; }
+
+        public string arpgroup1 = "arpgroup1";
+        public string arpgroup2 = "arpgroup2";
+        public int httpgroup1 = 1;
+        public int httpgroup2 = 2;
 
         public TestDataGrid()
         {
-            vl = new ObservableCollection<tdgviewitem>();
+            vl = new ObservableCollection<tdgitem>();
+            axes = new ObservableCollection<tdggroupingaxis>();
+
             InitializeComponent();
-            tdg.DataContext = this;
+            tdggrid.DataContext = this;
 
-            l = new tdgitemlist();
-            l.Add(new tdgitem("001", "192.168.11.222", "arp", "arp",  l));
-            l.Add(new tdgitem("002", "192.168.11.223", "arp", "arp",  l));
-            l.Add(new tdgitem("003", "192.168.11.224", "arp", "arp",  l));
-            l.Add(new tdgitem("004", "192.168.11.222", "dns", "http", l));
-            l.Add(new tdgitem("005", "192.168.11.223", "tcp", "tcp",  l));
-            l.Add(new tdgitem("006", "192.168.11.222", "tcp", "http", l));
-            l.Add(new tdgitem("007", "192.168.11.222", "tcp", "http", l));
-            l.Add(new tdgitem("008", "192.168.11.224", "arp", "arp",  l));
-            l.Add(new tdgitem("009", "192.168.11.222", "tcp", "http", l));
-            l.Add(new tdgitem("010", "192.168.11.223", "arp", "arp",  l));
-            l.Add(new tdgitem("011", "192.168.11.223", "arp", "arp",  l));
-            l.Add(new tdgitem("012", "192.168.11.225", "arp", "arp",  l));
-            l.Add(new tdgitem("013", "192.168.11.226", "arp", "arp",  l));
-            l.Add(new tdgitem("014", "192.168.11.222", "arp", "arp",  l));
-            l.Add(new tdgitem("015", "192.168.11.222", "tcp", "http", l));
-            l.Add(new tdgitem("016", "192.168.11.222", "tcp", "http", l));
-            l.Add(new tdgitem("017", "192.168.11.222", "tcp", "http", l));
-            l.Add(new tdgitem("018", "192.168.11.222", "tcp", "http", l));
+            vl.Add(new tdgitem("001", "192.168.11.222", "arp", arpgroup1,  vl));
+            vl.Add(new tdgitem("002", "192.168.11.223", "arp", arpgroup1,  vl));
+            vl.Add(new tdgitem("003", "192.168.11.224", "arp", arpgroup1,  vl));
+            vl.Add(new tdgitem("004", "192.168.11.222", "dns", httpgroup1, vl));
+            vl.Add(new tdgitem("005", "192.168.11.223", "tcp", httpgroup1, vl));
+            vl.Add(new tdgitem("006", "192.168.11.222", "tcp", httpgroup1, vl));
+            vl.Add(new tdgitem("007", "192.168.11.222", "tcp", httpgroup1, vl));
+            vl.Add(new tdgitem("008", "192.168.11.224", "arp", arpgroup2,  vl));
+            vl.Add(new tdgitem("009", "192.168.11.222", "tcp", httpgroup2, vl));
+            vl.Add(new tdgitem("010", "192.168.11.223", "arp", arpgroup2,  vl));
+            vl.Add(new tdgitem("011", "192.168.11.223", "arp", arpgroup2,  vl));
+            vl.Add(new tdgitem("012", "192.168.11.225", "arp", arpgroup2,  vl));
+            vl.Add(new tdgitem("013", "192.168.11.226", "arp", arpgroup2,  vl));
+            vl.Add(new tdgitem("014", "192.168.11.222", "arp", arpgroup2,  vl));
+            vl.Add(new tdgitem("015", "192.168.11.222", "tcp", httpgroup2, vl));
+            vl.Add(new tdgitem("016", "192.168.11.222", "tcp", httpgroup2, vl));
+            vl.Add(new tdgitem("017", "192.168.11.222", "tcp", httpgroup2, vl));
+            vl.Add(new tdgitem("018", "192.168.11.222", "tcp", httpgroup2, vl));
 
+            axes.Add(new tdggroupingaxis("grouped_proto", "Protocol", axes));
+            axes.Add(new tdggroupingaxis("grouped_ip", "IP Address", axes));
+            axes.Add(new tdggroupingaxis("grouped_group", "Group", axes));
 
-            foreach (tdgitem t in l) vl.Add(new tdgviewitem(t));
 
             // next line gets view on vl, not on tdg.Itemssource
             // at this point in execution, tdg.Itemssource is still null,
